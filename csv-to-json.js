5a34cf24600bc4d5d091c3c5639250a41faee6d2
Abdulrehman Ahmad <abdulrehman821337@gmail.com>
2023-12-28 15:12:01 +1100

    finish file

---
+++ csv-to-json.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+
+const results = [];
+fs.createReadStream("veg-pantry.csv")
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    fs.writeFileSync("veg-pantry.json", JSON.stringify(results, null, 4));
+  });
